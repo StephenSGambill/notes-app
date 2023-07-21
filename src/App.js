cd5b4f00a8f93a1610e90c5cd142e896649e3b4e
Stephen Gambill <stephensgambill@gmail.com>
2023-07-21 09:58:45 -0500

    a couple of tweaks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import { useEffect } from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import { nanoid } from "nanoid"
 import "./App.css"
@@ -14,8 +13,10 @@ export default function App() {
   // NB: the above addition of () => ensures that this runs only at the beginning when the app loads, not every time the page re-renders
   //This is called lazy State Initialization
   const [currentNoteId, setCurrentNoteId] = React.useState(
-    (notes[0] && notes[0].id) || ""
+    (notes[0]?.id) || ""
   )
+  const currentNote = notes.find(note => note.id === currentNoteId)
+    || notes[0]
 
 
   useEffect(() => {
@@ -34,7 +35,7 @@ export default function App() {
 
   function updateNote(text) {
     //next 4 line implements functionality of putting edited note at top of sidebar listing by finding the current note, making an array without it, then unshifting to put it on the front of the array, then setting notes to that new array
-    const currentNote = findCurrentNote()
+    // const currentNote = findCurrentNote()
     const newNoteArray = notes.filter(note => note.id !== currentNote.id)
     newNoteArray.unshift(currentNote)
     setNotes(newNoteArray)
@@ -45,12 +46,12 @@ export default function App() {
         : oldNote
     }))
   }
-
-  function findCurrentNote() {
-    return notes.find(note => {
-      return note.id === currentNoteId
-    }) || notes[0]
-  }
+  //replaced this function which was called 3x to a variable definition above and replaced function call with variable name
+  // function findCurrentNote() {
+  //   return notes.find(note => {
+  //     return note.id === currentNoteId
+  //   }) || notes[0]
+  // }
 
   function deleteNote(event, noteId) {
     event.stopPropagation()
@@ -70,7 +71,7 @@ export default function App() {
           >
             <Sidebar
               notes={notes}
-              currentNote={findCurrentNote()}
+              currentNote={currentNote}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
               handleDelete={deleteNote}
@@ -79,7 +80,7 @@ export default function App() {
               currentNoteId &&
               notes.length > 0 &&
               <Editor
-                currentNote={findCurrentNote()}
+                currentNote={currentNote}
                 updateNote={updateNote}
               />
             }
